5c86c91d2113fc6be56c061d7a012883ea6ed5e0
Tommy Lee <tommylee@example.com>
2026-04-17 19:40:48 -0700

    feat(ui): make note tape draggable to solve fullscreen video dragging

--- js/changelog.js
+++ js/changelog.js
@@ -4,7 +4,7 @@
 PostIt.Changelog = (function () {
     'use strict';
 
-    const CURRENT_VERSION = '2.1.31';
+    const CURRENT_VERSION = '2.1.32';
     const STORAGE_KEY = 'postit_last_seen_version';
 
     function init() {
