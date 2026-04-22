da60594c48f0d04dbca9ea4225382bc9b279f1de
Tommy Lee <tommylee@example.com>
2026-04-22 00:03:32 -0700

    style: 將系統公告便利貼左下角時間戳記字體改為白色並推出版號 2.1.47

--- js/changelog.js
+++ js/changelog.js
@@ -4,7 +4,7 @@
 PostIt.Changelog = (function () {
     'use strict';
 
-    const CURRENT_VERSION = '2.1.45';
+    const CURRENT_VERSION = '2.1.47';
     const STORAGE_KEY = 'postit_last_seen_version';
 
     function init() {
