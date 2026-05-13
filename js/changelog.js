c442ca5cad9e1862521eac1419b22c16a9c21a35
Tommy Lee <tommylee@example.com>
2026-05-12 23:33:03 -0700

    chore(release): update to v2.5.6 - 更新 style 模組

--- js/changelog.js
+++ js/changelog.js
@@ -4,7 +4,7 @@
 PostIt.Changelog = (function () {
     'use strict';
 
-    const CURRENT_VERSION = '2.5.5';
+    const CURRENT_VERSION = '2.5.6';
     const STORAGE_KEY = 'postit_last_seen_version';
 
     function init() {
