49b800d77b890e5762c772429bff4a34cfe21628
Tommy Lee <tommylee@example.com>
2026-05-14 02:16:17 -0700

    chore(release): update to v2.7.0 - 替換全站 Logo 為新的 3D 立體設計版，完美保持原 Layout 高度

--- js/changelog.js
+++ js/changelog.js
@@ -4,7 +4,7 @@
 PostIt.Changelog = (function () {
     'use strict';
 
-    const CURRENT_VERSION = '2.6.2';
+    const CURRENT_VERSION = '2.7.0';
     const STORAGE_KEY = 'postit_last_seen_version';
 
     function init() {
