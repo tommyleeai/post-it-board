377c85e594dd8060b4d437dd12d3b3f8fe404ad4
Tommy Lee <tommylee@example.com>
2026-05-14 03:08:26 -0700

    chore(release): update to v2.7.2 - 更換 Loading 畫面影片為黑色底圖版，完美支援 screen 混合模式去背

--- js/changelog.js
+++ js/changelog.js
@@ -4,7 +4,7 @@
 PostIt.Changelog = (function () {
     'use strict';
 
-    const CURRENT_VERSION = '2.7.1';
+    const CURRENT_VERSION = '2.7.2';
     const STORAGE_KEY = 'postit_last_seen_version';
 
     function init() {
