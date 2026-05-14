e6f3dbecd8582e51c00d7f73fa9c506adb01265b
Tommy Lee <tommylee@example.com>
2026-05-14 03:03:50 -0700

    chore(release): update to v2.7.1 - 替換全站 Logo 為與深色背景完美融合的新版 AI 生成圖

--- js/changelog.js
+++ js/changelog.js
@@ -4,7 +4,7 @@
 PostIt.Changelog = (function () {
     'use strict';
 
-    const CURRENT_VERSION = '2.7.0';
+    const CURRENT_VERSION = '2.7.1';
     const STORAGE_KEY = 'postit_last_seen_version';
 
     function init() {
